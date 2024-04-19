2d3fa60ca20d586f54e761823d6cb3229c9f0b6a
Sahe20 <jackitehm@gmail.com>
2024-04-19 17:18:40 +0300

    F to C function defined

--- temperature-converter.py
+++ temperature-converter.py
@@ -7,3 +7,5 @@ def converter_celsius_to_fahrenheit():
         label_result.config(text=f"{fahrenheit}\u00b0C")
     except ValueError:
         label_result.config(text="Invalid input, # only \U0001F609")
+
+def convert_fahrenheit_to_celsius():
