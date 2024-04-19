8f512c04d1685afb533be8421896efe9e8c9075a
Sahe20 <jackitehm@gmail.com>
2024-04-19 17:16:39 +0300

    C to F code written with exception handling

--- temperature-converter.py
+++ temperature-converter.py
@@ -1,3 +1,9 @@
 import tkinter as tk
 
 def converter_celsius_to_fahrenheit(): 
+    try: 
+        celsius = float(entry_celsius.get())
+        fahrenheit = (celsius * 9/5) + 32
+        label_result.config(text=f"{fahrenheit}\u00b0C")
+    except ValueError:
+        label_result.config(text="Invalid input, # only \U0001F609")
